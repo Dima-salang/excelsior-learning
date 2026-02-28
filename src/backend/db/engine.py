b191b9b92f390f3ed406e3e93f28247f1a88b0f7
Dima-salang <luisgabrielle1026@gmail.com>
2026-02-28 22:18:04 +0800

    add engine file

---
+++ src/backend/db/engine.py
@@ -0,0 +1,14 @@
+from sqlmodel import SQLModel, create_engine
+from models.user import User
+from models.lecture import Lecture
+from models.lecture_section import LectureSection
+from models.lecture_step import LectureStep
+from models.deck import Deck
+from models.card import Card
+
+sqlite_file_name = "db.sqlite"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine = create_engine(sqlite_url, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
