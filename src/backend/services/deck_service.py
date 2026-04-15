af2fd463a31394373120398c9b01a6cd71b30a63
Dima-salang <luisgabrielle1026@gmail.com>
2026-04-15 16:40:06 +0800

    modified deck service

--- src/backend/services/deck_service.py
+++ src/backend/services/deck_service.py
@@ -1,6 +1,7 @@
 from sqlmodel import Session, select
-from models.deck import Deck
-from models.card import Card
+from sqlalchemy.orm import selectinload
+from models.deck import Deck, DeckPublic, DeckPublicWithCards
+from models.card import Card, CardPublic
 from typing import List
 
 
@@ -8,17 +9,21 @@ class DeckService:
     def __init__(self, session: Session):
         self.session = session
 
-    def get_decks(self, user_id: int) -> List[Deck]:
+    def get_decks(self, user_id: int) -> List[DeckPublic]:
         statement = (
             select(Deck).where(Deck.user_id == user_id).order_by(Deck.created_at.desc())
         )
         results = self.session.exec(statement)
         return results.all()
 
-    def get_deck(self, deck_id: int) -> Deck:
-        return self.session.get(Deck, deck_id)
+    def get_deck(self, deck_id: int) -> DeckPublicWithCards | None:
+        statement = (
+            select(Deck).where(Deck.id == deck_id).options(selectinload(Deck.cards))
+        )
+        result = self.session.exec(statement)
+        return result.first()
 
-    def get_cards(self, deck_id: int) -> List[Card]:
+    def get_cards(self, deck_id: int) -> List[CardPublic]:
         statement = select(Card).where(Card.deck_id == deck_id)
         results = self.session.exec(statement)
         return results.all()
